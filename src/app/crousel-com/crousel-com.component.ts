13699db5f5a25796377c22cc587d6151315c3c3f
Taranjit Singh <taranjit.singh97@punjab.gov.in>
2024-06-10 22:53:17 +0530

    first commit

---
+++ src/app/crousel-com/crousel-com.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crousel-com',
+  templateUrl: './crousel-com.component.html',
+  styleUrls: ['./crousel-com.component.scss']
+})
+export class CrouselComComponent {
+
+}
